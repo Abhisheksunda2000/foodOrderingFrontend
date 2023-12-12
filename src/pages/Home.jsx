d38805af127d89f704e75b05020acec8d3ec998b
Abhishek Choudhary <abhishek.choudhary@cogoport.com>
2023-12-12 07:58:22 +0530

    basic front done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
+import Carousel from '../components/Carousel';
 export default function Home() {
   return (
     <>
         <Navbar />
+        <div><Carousel /></div>
+        <div>
+          <Card />
+        </div>
+        <Footer />
     </>
   )
 }
