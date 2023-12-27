1a7e308f55d5b7b2db5311f8af8237b7d79c0843
Abhishek Choudhary <abhishek.choudhary@cogoport.com>
2023-12-27 16:33:56 +0530

    update backend url

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { CartProvider } from "./components/ContextReducer";
 import MyOrders from "./pages/MyOrders";
-
 
 function App() {
   return (
     <>
-    <CartProvider>
-      <Router>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/Login" element={<Login />} />
-            <Route exact path="/createuser" element={<Signup />} />
-            <Route exact path="/myOrders" element={<MyOrders />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+      <CartProvider>
+        <Router>
+          <div>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/Login" element={<Login />} />
+              <Route exact path="/createuser" element={<Signup />} />
+              <Route exact path="/myOrders" element={<MyOrders />} />
+            </Routes>
+          </div>
+        </Router>
+      </CartProvider>
     </>
   );
 }
